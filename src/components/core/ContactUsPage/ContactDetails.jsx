67bb00443b3f5f4c651da131e8ba160b7e9dc601
Surajsdeo1 <surajsinghdeo9@gmail.com>
2024-04-14 12:49:06 +0530

    only images are not upload .all work is completed

--- src/components/core/ContactUsPage/ContactDetails.jsx
+++ src/components/core/ContactUsPage/ContactDetails.jsx
@@ -8,20 +8,20 @@ const contactDetails = [
     icon: "HiChatBubbleLeftRight",
     heading: "Chat on us",
     description: "Our friendly team is here to help.",
-    details: "info@studynotion.com",
+    details: "surajsinghdeo9@gmail.com",
   },
   {
     icon: "BiWorld",
     heading: "Visit us",
     description: "Come and say hello at our office HQ.",
     details:
-      "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+      "Gujrati Sanatan near women college, Bistupur, Jharkhand-833219",
   },
   {
     icon: "IoCall",
     heading: "Call us",
     description: "Mon - Fri From 8am to 5pm",
-    details: "+123 456 7869",
+    details: "+91-6287904566",
   },
 ]
 
